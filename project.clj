3144bad82de2e32e665ebeb595413aaa0c2559fa
aristotelesbr <aristotelesbr@gmail.com>
2022-02-21 09:24:07 -0300

    add/config migratus

--- project.clj
+++ project.clj
@@ -2,10 +2,20 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
+  :migratus {:store :database
+             :migration-dir "migrations"
+             :db {:classname "org.postgresql.Driver"
+                  :subprotocol "postgresql"
+                  :subname "//localhost:5432/invoice_api_dev"
+                  :user "aristotelescosta"
+                  :password "postgres"}}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.6.1"]
-                 [ring/ring-defaults "0.3.2"]]
-  :plugins [[lein-ring "0.12.5"]]
+                 [ring/ring-defaults "0.3.2"]
+                 [migratus "1.3.5"]
+                 [org.postgresql/postgresql "42.1.4"]]
+  :plugins [[lein-ring "0.12.5"]
+            [migratus-lein "0.7.3"]]
   :ring {:handler invoice-api.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
